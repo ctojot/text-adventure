8f016f7665e6c6798a71f48f76170b9572947f15
ChristianRhey Tojot <christiantojot@gmail.com>
2024-04-02 18:12:11 -0700

    started creating file and story line

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  std::cout << "The year is 2024. You just fired from work.\n";
+  std::cout << "You just got dumped by your significant other.\n";
+  std::cout << "The landlord is calling you cause you're 3 months behind on rent.\n";
+  std::cout << "You sit there sli";
   return 0;
 }
